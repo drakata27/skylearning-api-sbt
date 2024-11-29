f9b4a9aee0f68549e3e3a433e4c4bca2835053f3
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-11-29 10:08:50 +0000

    Refactored code

--- src/main/java/org/aleksdraka/skylearningbackend/config/SecurityConfig.java
+++ src/main/java/org/aleksdraka/skylearningbackend/config/SecurityConfig.java
@@ -14,31 +14,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authorizeRequests ->
-//                        authorizeRequests.anyRequest().authenticated())
-//                .oauth2Login(oauth2Login ->
-//                    oauth2Login.defaultSuccessUrl("http://localhost:3000", true))
-//                .logout(logout ->
-//                        logout
-//                                .logoutUrl("/oauth/logout")
-//                                .logoutSuccessUrl("http://localhost:3000")
-//                                .invalidateHttpSession(true)
-//                                .deleteCookies("JSESSIONID"));
-//        return http.build();
-//    }
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/sections/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/sections/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/sections/**").authenticated()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2Login ->
                         oauth2Login.defaultSuccessUrl("http://localhost:3000", true))
