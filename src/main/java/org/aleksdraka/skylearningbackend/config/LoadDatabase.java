ac0dd2503cbfbdbec5938f9d83440b42e7193670
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-11-30 16:55:57 +0000

    Users see their sections only

--- src/main/java/org/aleksdraka/skylearningbackend/config/LoadDatabase.java
+++ src/main/java/org/aleksdraka/skylearningbackend/config/LoadDatabase.java
@@ -8,18 +8,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class LoadDatabase {
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+//    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-//    @Bean
-//    CommandLineRunner initDatabase(SectionService sectionService) {
-//        return _ -> {
-//            sectionService.saveSection(new Section("Learn AWS","Improve your knowledge"));
+    @Bean
+    CommandLineRunner initDatabase(SectionService sectionService) {
+        return _ -> {
+            sectionService.saveSection(new Section("User ID Test","Google Account", "107411559235337526170"));
 //            sectionService.saveSection(new Section("Java Notes","My notes"));
 //            sectionService.saveSection(new Section("Python Notes","Python Notes"));
-//
+
 //            sectionService.getAllSections().forEach(section -> log.info("Preloaded {}", section));
-//        };
-//    }
+        };
+    }
 }
---
+++ src/main/resources/db/V2_AddColumn.sql
@@ -0,0 +1,2 @@
+ALTER TABLE section
+ADD COLUMN user_id varchar(255);
