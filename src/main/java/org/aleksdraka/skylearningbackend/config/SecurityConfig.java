00d0844ea11603944a85a64d4364ab2e133701f1
Aleksandar Drakaliyski <aleksandardrakaliyski@Mac.broadband>
2024-12-03 11:39:32 +0000

    Notes can be viewed

--- src/main/java/org/aleksdraka/skylearningbackend/config/SecurityConfig.java
+++ src/main/java/org/aleksdraka/skylearningbackend/config/SecurityConfig.java
@@ -18,7 +18,6 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .anyRequest().authenticated())
-//                        .anyRequest().permitAll())
                 .oauth2Login(oauth2Login ->
                         oauth2Login.defaultSuccessUrl("http://localhost:3000", true))
                 .logout(logout ->
